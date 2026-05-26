08dc71a71c9d3eae96bc882bd0707200b2b3f3ba
eduardo <erustick11@gmail.com>
2026-05-25 21:57:03 -0300

    ajustes

--- src/routes/private.routes.jsx
+++ src/routes/private.routes.jsx
@@ -4,11 +4,8 @@ import { Home } from '../pages/Home';
 import { TelaDoador } from '../pages/TelaDoador';
 import { TelaRecebedor } from '../pages/TelaRecebedor';
 import { EditarPerfil } from '../pages/EditarPerfil';
-<<<<<<< Updated upstream
 import { Chat } from '../pages/Chat';
-=======
 import { TelaHistorico } from '../pages/TelaHistorico';
->>>>>>> Stashed changes
 
 export function PrivateRoutes() {
   return (
@@ -16,11 +13,8 @@ export function PrivateRoutes() {
       <Route path="/doador" element={<TelaDoador />} />
       <Route path="/recebedor" element={<TelaRecebedor />} />
       <Route path="/perfil" element={<EditarPerfil />} />
-<<<<<<< Updated upstream
       <Route path="/chat/:solicitacaoId" element={<Chat />} />
-=======
       <Route path="/historico" element={<TelaHistorico />} />
->>>>>>> Stashed changes
     </Routes>
   );
 }
